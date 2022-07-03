6071f3e1fa513d5f264726e2a6fa21b7405ebb79
zeppo <zeppocode@gmail.com>
2022-07-02 22:00:44 -0400

    to add Run class in Models

---
+++ RunningFeverApp/Models/Run.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+public class Run
+{
+    [Key]
+    public int Id { get; set; } 
+    public DateTime StartTime {get; set;}
+    public int Miles  {get; set;}
+    public string? Route {get; set;}
+    public string? Note {get; set;}
+
+}
