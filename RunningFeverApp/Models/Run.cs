0b15426b81bfce554a6946854e8c63ef95750281
zeppo <zeppocode@gmail.com>
2022-07-25 20:43:21 -0400

    change miles type to float

--- RunningFeverApp/Models/Run.cs
+++ RunningFeverApp/Models/Run.cs
@@ -5,7 +5,7 @@ public class Run
     [Key]
     public int Id { get; set; } 
     public DateTime StartTime {get; set;}
-    public int Miles  {get; set;}
+    public float Miles  {get; set;}
     public string? Route {get; set;}
     public string? Note {get; set;}
 
